eb4fef58f1ab26098b3a309c03fc9ba61eda48d2
nikhilpatil2012 <patilnikhil9@gmail.com>
2014-09-08 12:07:39 +0530

    Update SharedPrefrenceStorage.java

--- SharedPrefrenceStorage.java
+++ SharedPrefrenceStorage.java
@@ -3,6 +3,7 @@ package tronbox.welcome;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import tronbox.Logs.ErrorLogs;
 
@@ -13,14 +14,16 @@ public class SharedPrefrenceStorage {
         return context.getSharedPreferences("Quizzy", Context.MODE_PRIVATE);
     }
 
-    public static void StoreProfileInfo(Context context, String FirstName, String LastName, String UserCode, String FacebookId) {
+    public static void StoreProfileInfo(Context context, String FirstName, String FacebookId, String UserLocation, String UserDob, String gender) {
 
          SharedPreferences.Editor editor = initialize(context).edit();
          editor.putString("Name", FirstName);
-         editor.putString("LastName", LastName);
-         editor.putString("UserCode", UserCode);
          editor.putString("FacebookId", FacebookId);
          editor.putBoolean("Login", true);
+         editor.putString("Location", UserLocation);
+         editor.putString("Dob", UserDob);
+         editor.putString("Gender", gender);
+
          editor.commit();
 
         ErrorLogs.PARSE_ERRORS(ErrorLogs.SharedPrefrenceLog, "PROFILE DATA SUCCESSFULLY STORED IN THE SHARED_PREFRENCE "+getUserCode(context));
@@ -70,10 +73,21 @@ public class SharedPrefrenceStorage {
 
         Bundle bundle = new Bundle();
         bundle.putString("Name", preferences.getString("Name", ""));
-        bundle.putString("UserCode", preferences.getString("UserCode", ""));
         bundle.putString("FacebookId", preferences.getString("FacebookId", ""));
+        bundle.putString("Location", preferences.getString("Location", ""));
+        bundle.putString("Dob", preferences.getString("Dob", ""));
+        bundle.putString("Gender", preferences.getString("Gender", ""));
 
-         return  bundle;
+
+        return  bundle;
+    }
+
+    public static void storeUserCode(Context context, String usercode){
+
+        SharedPreferences.Editor editor = initialize(context).edit();
+        editor.putString("UserCode", usercode);
+        editor.commit();
+
     }
 
     public static String getUserCode(Context context){
@@ -89,5 +103,44 @@ public class SharedPrefrenceStorage {
         return initialize(context).getString("FacebookId", "");
     }
 
+    public static boolean saveUpdateInfo(Context context,String name,String mobile,String gender,String bday,String bmonth,String byear){
+
+        SharedPreferences.Editor editor = initialize(context).edit();
+        editor.putString("Name", name);
+        editor.putString("userMobile", mobile);
+        editor.putString("userGender", gender);
+        editor.putString("birthDay", bday);
+        editor.putString("birthMonth", bmonth);
+        editor.putString("birthYear", byear);
+        return editor.commit();
+    }
+
+    public static Bundle getProfileData(Context context){
+        SharedPreferences preferences = initialize(context);
+        Bundle bundle = new Bundle();
+        bundle.putString("Name", preferences.getString("Name", ""));
+        bundle.putString("FacebookId", preferences.getString("FacebookId", ""));
+        bundle.putString("userMobile", preferences.getString("userMobile", ""));
+        bundle.putString("userGender", preferences.getString("userGender", ""));
+        bundle.putString("birthDay", preferences.getString("birthDay", ""));
+        bundle.putString("birthMonth", preferences.getString("birthMonth", ""));
+        bundle.putString("birthYear", preferences.getString("birthYear", ""));
+        return bundle;
+    }
+
+    public static void storeHomeState(Context context, int state){
+
+        Log.w("HomeState", ""+state);
+
+        SharedPreferences.Editor editor = initialize(context).edit();
+        editor.putInt("State", state);
+        editor.commit();
+
+    }
+
+    public static int getCurrentState(Context context){
+
+        return initialize(context).getInt("State", 0);
+    }
 
 }
