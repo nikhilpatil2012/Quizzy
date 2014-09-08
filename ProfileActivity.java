82ab3de7702dc2f0067d207482173c1c2da026d7
nikhilpatil2012 <patilnikhil9@gmail.com>
2014-09-08 15:58:13 +0530

    Update ProfileActivity.java

--- ProfileActivity.java
+++ ProfileActivity.java
@@ -1,20 +1,42 @@
+package tronbox.welcome;
+
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import dev.support.SharedPrefrenceStorage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import tronbox.social.R;
+
 
 public class ProfileActivity extends Activity {
 
     private EditText name,mobile;
     private RadioGroup radioGroup;
     private DatePicker datePicker;
+    private ImageView proImageView;
     private Button submit;
     private String usr_name,usr_mobile,usr_bday,usr_bmonth,usr_byear,usr_gender;
 
@@ -22,19 +44,29 @@ public class ProfileActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         setContentView(R.layout.activity_profile);
 
-        getActionBar().hide();
+        initActionBar(getActionBar());
 
-        Bundle b = SharedPrefrenceStorage.getProfileData(getApplicationContext());
+        Bundle b = SharedPrefrenceStorage.getProfileInfo(getApplicationContext());
 
-        init(b.getString("Name"),b.getString("userMobile"),b.getString("userGender"),b.getString("birthDay"),b.getString("birthMonth"),b.getString("birthYear"));
+        init(b.getString("Name"),"-",b.getString("Gender"),b.getString("Dob"),b.getString("FacebookId"));
 
 
     }
 
-    private void init(String u_name,String u_mobile,String u_gender,String ubday,String ubmonth,String u_byear)
+    private void init(String u_name,String u_mobile,String u_gender,String udob,String u_code)
     {
+        proImageView = (ImageView)findViewById(R.id.profilePic);
+        try
+        {
+
+            proImageView.setImageBitmap(BitmapFactory.decodeStream(openFileInput(u_code+".png")));
+        }catch (Exception e){
+
+        }
+
         name = (EditText)findViewById(R.id.name);
         mobile = (EditText)findViewById(R.id.mobile);
         datePicker = (DatePicker)findViewById(R.id.datePicker1);
@@ -45,11 +77,19 @@ public class ProfileActivity extends Activity {
         name.setText(u_name);
         mobile.setText(u_mobile);
 
-        try {
-            datePicker.updateDate(Integer.valueOf(u_byear), Integer.valueOf(ubmonth), Integer.valueOf(ubday));
-        }catch (Exception e){
+        if(!udob.equals("null")){
 
+            String dob[] = udob.split("/");
+            Log.w("DOB", ""+dob[0]+"\n"+dob[1]+"\n"+dob[2]);
+
+            try {
+                datePicker.updateDate(Integer.valueOf(dob[2]), Integer.valueOf(dob[0])-1, Integer.valueOf(dob[1]));
+            }catch (Exception e){
+
+            }
         }
+
+
 
         if(u_gender.equals("male"))
         {
@@ -68,10 +108,21 @@ public class ProfileActivity extends Activity {
                 getAllValuesAndSave();
             }
         });
+
+        proImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                final int ACTIVITY_SELECT_IMAGE = 1235;
+                startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+            }
+        });
     }
 
     private void getAllValuesAndSave()
     {
+
         usr_name = name.getText().toString();
         usr_mobile = mobile.getText().toString();
         usr_bday = ""+datePicker.getDayOfMonth();
@@ -98,4 +149,94 @@ public class ProfileActivity extends Activity {
 
     }
 
+    private void initActionBar(ActionBar actionBar)
+    {
+        actionBar.setTitle("Login");
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        LayoutInflater mInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View mCustomView = mInflater.inflate(R.layout.custom_action_bar_score_activity, null);
+
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        mCustomView.setLayoutParams(params);
+        actionBar.setCustomView(mCustomView);
+        TextView actionBarText = (TextView)mCustomView.findViewById(R.id.nameOfScreen);
+        actionBarText.setText("Profile");
+
+        TextView back = (TextView)mCustomView.findViewById(R.id.back);
+        back.setText("Home");
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), MasterHomeScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
+        actionBar.setDisplayShowCustomEnabled(true);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1235:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    final String filePath = cursor.getString(columnIndex);
+                    cursor.close();
+                    proImageView.getHandler().post(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Log.w("DEMO", "path >> " + filePath);
+                                ExifInterface exif = new ExifInterface(filePath);
+                                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
+                                Log.w("DEMO", "ORIENTATION :: " + orientation);
+                                Bitmap image = BitmapFactory.decodeFile(filePath);
+                                if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                                    Matrix mat = new Matrix();
+                                    mat.postRotate(90);
+                                    image = Bitmap.createBitmap(image, 0, 0, image.getWidth(), image.getHeight(), mat, true);
+                                }
+                                if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                                    Matrix mat = new Matrix();
+                                    mat.postRotate(180);
+                                    image = Bitmap.createBitmap(image, 0, 0, image.getWidth(), image.getHeight(), mat, true);
+                                }
+                                if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                                    Matrix mat = new Matrix();
+                                    mat.postRotate(270);
+                                    image = Bitmap.createBitmap(image, 0, 0, image.getWidth(), image.getHeight(), mat, true);
+                                }
+
+                                proImageView.setImageBitmap(Bitmap.createScaledBitmap(image,proImageView.getWidth(),proImageView.getHeight(),true));
+
+                                File f = new File(SharedPrefrenceStorage.getUserFacebookId(getApplicationContext())+".png");
+                                FileOutputStream out = openFileOutput(f.getName(), Context.MODE_PRIVATE);
+                                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                                image.compress(Bitmap.CompressFormat.PNG, 100, bos);
+                                out.write(bos.toByteArray(), 0, bos.toByteArray().length);
+                                out.close();
+
+                            } catch (OutOfMemoryError e) {
+                                Toast.makeText(getApplicationContext(),"Error in updating image",Toast.LENGTH_LONG).show();
+
+                            } catch (Exception e) {
+
+                                Toast.makeText(getApplicationContext(),"Error in updating image",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+        }
+    }
 }
